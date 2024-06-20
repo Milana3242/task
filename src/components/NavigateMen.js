d54941f7593f23ceb520a39b638d65440c0c59a4
Milana <milana-deva@gmail.com>
2024-06-20 15:43:04 +0300

    addd

--- src/components/NavigateMen.js
+++ src/components/NavigateMen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavigateMenu() {
   const tasks = useSelector((state) => state.taskGroop);
   console.log(tasks.length === 0);
+  const navigate = useNavigate();
+
+  function openTask(name){
+    navigate(`my-app/src/pages/CheckPointsPage.js?name=${name}`);
+let nam=window.location.search.split('=')[1]
+  }
+
   return (
     <div className="navigate">
       <nav>
@@ -15,7 +23,7 @@ export default function NavigateMenu() {
           {tasks.length > 0 &&
             tasks.map((item) => {
               console.log(item.name);
-              return <li>{item.name}</li>;
+              return <li onClick={()=>openTask(item.name)}>{item.name}</li>;
             })}
         </ol>
       </nav>
--- src/pages/CheckPointsPage.js
+++ src/pages/CheckPointsPage.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import Task from '../components/Task';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import Task from "../components/Task";
+import { useSelector, useDispatch } from "react-redux";
 
 function CheckPointsPage(props) {
   const name = useSelector((state) => state.taskGroop.name);
-  console.log(useSelector((state) => state.taskGroop));
   const count = useSelector((state) => state.taskGroop.count);
-
   const tasks = useSelector((state) => state.taskGroop);
-
   return (
     <div>
       {tasks.length > 0 &&
